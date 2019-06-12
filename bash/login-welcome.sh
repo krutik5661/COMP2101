545ce366b609e4d8f63ca5ed9b2ad3325a86e05a
krutik5661 <50630925+krutik5661@users.noreply.github.com>
2019-06-11 22:03:00 -0400

    new file

---
+++ bash/login-welcome.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#
+# This script produces the dynamic welcome message
+# it should look like
+#   Welcome to planet hostname, title name!
+
+# Improve this script by using the value in the automatic variable $USER for the name
+# Improve this script by adding some time and day of the week information to the welcome message
+#   Use a format like this:
+#   It is HH:MM AM on weekday.
+
+###############
+# Variables   #
+###############
+title="Overlord"
+USER="KRUTIK"
+hostname=$(hostname)
+time=$(date +'%H:%M %p on %A')
+
+Welcome=$(cat <<EOF
+Welcome to planet $hostname, "$title $USER!"
+
+It is $time
+EOF
+)
+
+###############
+# Main        #
+###############
+
+cat <<EOF
+$(cowsay "$Welcome")
+EOF
